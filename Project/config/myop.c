b2377c85f53e98f7a6dc58e7939910729e0568f7
zhangjingle <zhangjingle.love@163.com>
2016-06-06 22:19:34 +0800

    读配置文件的小项目，添加了一个测试案例 Signed-off-by: zhangjingle <zhangjingle.love@163.com>

---
+++ Project/config/myop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+#include "myop.h"
+
+int AddTeacher(char *pfileName, Teacher *pTe)
+{
+	
+}
+
+int ModifyTeacher(char *pfileName, Teacher *pTe)
+{
+	
+}
---
+++ Project/config/myop.h
@@ -0,0 +1,26 @@
+
+// cfg_op.h
+
+#ifndef _INC_MYOPT_H
+#define _INC_MYOPT_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+typedef struct Teacher
+{
+	char name[64];
+	int age;
+	char studentInfo[1024];
+}Teacher;
+
+int AddTeacher(char *pfileName, Teacher *pTe);
+
+int ModifyTeacher(char *pfileName, Teacher *pTe);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif 
